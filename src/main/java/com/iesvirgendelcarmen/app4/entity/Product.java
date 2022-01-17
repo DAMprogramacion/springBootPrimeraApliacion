904ed0469abde2970c92bd15470d231671311d55
programacionDAMVC <programacionDAMVC@gmail.com>
2022-01-17 10:39:00 +0100

    nuevo archivo:  .gitignore 	nuevo archivo:  .mvn/wrapper/maven-wrapper.jar 	nuevo archivo:  .mvn/wrapper/maven-wrapper.properties 	nuevo archivo:  README.md 	nuevo archivo:  mvnw 	nuevo archivo:  mvnw.cmd 	nuevo archivo:  pom.xml 	nuevo archivo:  src/main/java/com/iesvirgendelcarmen/app4/App4Application.java 	nuevo archivo:  src/main/java/com/iesvirgendelcarmen/app4/entity/Product.java 	nuevo archivo:  src/main/java/com/iesvirgendelcarmen/app4/repository/ProductRepository.java 	nuevo archivo:  src/main/resources/application.properties 	nuevo archivo:  src/test/java/com/iesvirgendelcarmen/app4/App4ApplicationTests.java

---
+++ src/main/java/com/iesvirgendelcarmen/app4/entity/Product.java
@@ -0,0 +1,61 @@
+package com.iesvirgendelcarmen.app4.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(length = 50)
+	private String name;
+	private Float price;
+	
+	public Product(String name, Float price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public Product() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Float getPrice() {
+		return price;
+	}
+
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Product [id=%s, name=%s, price=%s]", id, name, price);
+	}
+
+	
+	
+	
+	
+}
